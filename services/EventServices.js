eeee7e0cb6c6ed288c745870af700b4de26dff7c
Rodrigo Hernando <rhernandog@gmail.com>
2020-09-03 17:00:38 -0400

    Updated event service base URL

--- services/EventServices.js
+++ services/EventServices.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:4000/",
+  baseURL: "https://my-json-server.typicode.com/rhernandog/nuxtjs-first-project",
   withCredentials: false,
   headers: {
     Accept: "application/json",
